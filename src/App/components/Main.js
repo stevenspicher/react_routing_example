93e6a9f4fdda563645f5656f04b97adc86aeb978
steven <steven.spicher@gmail.com>
2021-10-28 08:58:39 -0600

    cleanup

--- src/App/components/Main.js
+++ src/App/components/Main.js
@@ -1,26 +1,22 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 
 import Home from './Header/Home';
 import About from './Header/About';
-import Error from './Error';
-
 
 
 const Main = () =>  {
     return (
         <main>
-            <Switch>
+            
                 <Route exact path='/'> 
                     <Home/>
                 </Route>
                 <Route path='/about'> 
                     <About/>
                 </Route>
-                <Route path='*'> 
-                    <Error/>
-                </Route>
-              </Switch>
+             
+             
    
         </main>
 

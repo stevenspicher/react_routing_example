63e1105c647ed2561a95e0049a09cd11ed418519
steven <steven.spicher@gmail.com>
2021-10-28 08:26:55 -0600

    fixed App again

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Main from './components/Main';
 import Header from './components/Header/Header';
 import Banner from './components/Banner/Banner';
-import Body from './components/Body/Body';
+
 
 
 

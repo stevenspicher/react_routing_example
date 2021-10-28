bcae106be098a50d97d47a0b1ec3b9aa997a901d
steven <steven.spicher@gmail.com>
2021-10-28 08:42:20 -0600

    error page

--- src/App/components/Main.js
+++ src/App/components/Main.js
@@ -3,20 +3,24 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './Header/Home';
 import About from './Header/About';
+import Error from './Error';
 
 
 
 const Main = () =>  {
     return (
         <main>
-
+            <Switch>
                 <Route exact path='/'> 
                     <Home/>
                 </Route>
                 <Route path='/about'> 
                     <About/>
                 </Route>
-              
+                <Route path='*'> 
+                    <Error/>
+                </Route>
+              </Switch>
 
                  
               
